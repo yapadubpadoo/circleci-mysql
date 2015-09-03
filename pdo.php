5f2aae20a43b8e9ae20bb0df9482749b06c4859d
yapadubpadoo <yapadubpadoo@gmail.com>
2015-09-04 00:32:42 +0700

    update pdo script

--- pdo.php
+++ pdo.php
@@ -2,7 +2,7 @@
 
 try 
 {
-	$dbh = new PDO('mysql:host=localhost;post:3306;');
+	$dbh = new PDO('mysql:host=localhost;post:3306;dbname=my_db', 'testuser', 'password');
 	print_r($dbh);
 } 
 catch (PDOException $e) 
