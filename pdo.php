67f898b4b4974984f6c1a003093abc5dc8f1e634
yapadubpadoo <yapadubpadoo@gmail.com>
2015-09-04 00:45:44 +0700

    update pdo script

--- pdo.php
+++ pdo.php
@@ -2,11 +2,11 @@
 
 try 
 {
-	$dbh = new PDO('mysql:host=127.0.0.1;post:3306;dbname=circle_test');
+	$dbh = new PDO('mysql:host=127.0.0.1;post:3306;dbname=circle_test', 'testuser', 'password');
 	print_r($dbh);
 } 
 catch (PDOException $e) 
 {
-	print "Error!: " . $e->getMessage() . "<br/>";
+	print "Error!: " . $e->getMessage() . "";
 	die();
 }
